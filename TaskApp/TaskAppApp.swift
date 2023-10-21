195b38bf4b8776e9a9d100764f22de341cdf25c1
Ramia-aloufi <ramia.aloufi@gmail.com>
2023-10-21 20:30:34 +0300

    Initial Commit

---
+++ TaskApp/TaskAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskAppApp.swift
+//  TaskApp
+//
+//  Created by ramia n on 06/04/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct TaskAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
